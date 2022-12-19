dd5a3048798e09e4cba280a1f5330f74367a76c4
Love Norman <90197758+lovenorman@users.noreply.github.com>
2022-12-19 15:04:40 +0000

    trying

--- app/scoreboard.ts
+++ app/scoreboard.ts
@@ -1,23 +1,24 @@
-///<reference path="result.ts" />
+/// <reference path="result.ts" />
 
 class Scoreboard {
+
     private results: Result[] = [];
-
-    addResult(newResult: Result):void {
-        this.results.push(newResult);
+  
+    addResult(newResult: Result): void {
+      this.results.push(newResult);
     }
-
+  
     updateScoreboard(): void {
-        let output: string= '<h2>Scoreboard</h2>';
-
-        for(let i = 0; i < this.results.length; i++){
-            const result: Result = this.results[i];
-            output+= '<h4>';
-            output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor' + result.factor;
-            output += '</h4>';
-        }
-
-        const scoresElement: HTMLElement= document.getElementById('scores')!;
-        scoresElement.innerHTML = output;
-    }
-}
+      let output: string = '<h2>Scoreboard</h2>';
+  
+      for (let index = 0; index < this.results.length; index++) {
+        const result: Result = this.results[index];
+        output += '<h4>';
+        output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor ' + result.factor;
+        output += '</h4>';
+      }
+  
+      const scoresElement: HTMLElement = document.getElementById('scores')!;
+      scoresElement.innerHTML = output;
+    }  
+  }
--- app/utility.ts
+++ app/utility.ts
@@ -1,6 +1,9 @@
-class Utility{
+class Utility {
+
     static getInputValue(elementID: string): string {
-        const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
-        return inputElement.value;
+  
+      const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+      return inputElement.value;
     }
-}
+  
+  }
